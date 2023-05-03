fd56158a7a76934d959ce1098ef2d3b727f38a44
Carlos <carlosdanieleso@gmail.com>
2023-05-03 10:33:06 +0200

    Proyecto inicial

---
+++ src/clasepersonacolaborativo/ClasePersona.java
@@ -0,0 +1,11 @@
+
+package clasepersonacolaborativo;
+
+import java.time.LocalDate;
+
+
+public class ClasePersona {
+    private String nombre;
+    private String apellidos;
+    private LocalDate fechaNacimiento;
+}
---
+++ src/clasepersonacolaborativo/GestionPersona.java
@@ -0,0 +1,12 @@
+
+package clasepersonacolaborativo;
+
+
+public class GestionPersona {
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
